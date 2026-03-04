a7a8bf8f10d61205e7e761acb8297e2b92faea5e
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2026-03-04 21:56:45 +0100

    [WIP] SafeBound module interface updates

--- postbound_extensions/safebound/__init__.py
+++ postbound_extensions/safebound/__init__.py
@@ -1,16 +1,17 @@
+from ._catalog import SafeBoundCatalog, SafeBoundSpec
 from ._compress import valid_compress
-from ._core import CumulativeDegreeSequence, DegreeSequence
+from ._core import DegreeSequence
 from ._estimator import SafeBoundEstimator
 from ._fdsb import AlphaStep, BetaStep, fdsb
-from ._piecewise_fns import PiecewiseConstantFn, PiecewiseLinearFn
+from ._piecewise_fns import PiecewiseConstantFn
 
 __all__ = [
     "AlphaStep",
     "BetaStep",
-    "CumulativeDegreeSequence",
     "DegreeSequence",
     "PiecewiseConstantFn",
-    "PiecewiseLinearFn",
+    "SafeBoundSpec",
+    "SafeBoundCatalog",
     "SafeBoundEstimator",
     "fdsb",
     "valid_compress",
