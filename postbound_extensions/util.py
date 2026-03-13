e94187660c0798c434ceb538a5e4c4f0eab8a8ff
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2026-03-13 10:48:07 +0100

    Create JSON parsing utility

--- postbound_extensions/util.py
+++ postbound_extensions/util.py
@@ -1,5 +1,9 @@
 from __future__ import annotations
 
+from collections.abc import Callable
+from datetime import date, datetime
+from typing import Any
+
 import pandas as pd
 import postbound as pb
 from torch.utils.data import Dataset
@@ -35,3 +39,23 @@ class PandasDataset(Dataset):
 
     def __getitem__(self, idx: int):
         return list(self.df.iloc[idx])
+
+
+def _noop_parser[T](value: T) -> T:
+    return value
+
+
+def _date_parser(d: str) -> date:
+    return datetime.fromisoformat(d).date()
+
+
+def make_json_parser(column_dtype: str) -> Callable[[Any], Any]:
+    match column_dtype:
+        case "timestamp with time zone" | "timestamp without time zone":
+            return datetime.fromisoformat
+        case "date":
+            return _date_parser
+        case "varchar" | "text" | "integer":
+            return _noop_parser
+        case _:
+            raise ValueError(f"Missing JSON parser for column type {column_dtype}")
