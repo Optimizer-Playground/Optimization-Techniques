[Bao] Fix exploding gradients due to missing log-scales

postbound_extensions/bao/_featurizer.py
@@ -90,12 +90,8 @@ class BinarizedQep:
 
         elif plan.is_join():
             assert plan.outer_child and plan.inner_child
-            binarized_outer = BinarizedQep.create_for(
-                plan.outer_child, cache_state=cache_state
-            )
-            binarized_inner = BinarizedQep.create_for(
-                plan.inner_child, cache_state=cache_state
-            )
+            binarized_outer = BinarizedQep.create_for(plan.outer_child, cache_state=cache_state)
+            binarized_inner = BinarizedQep.create_for(plan.inner_child, cache_state=cache_state)
             return BinarizedQep.pseudo_join(
                 plan.node_type,
                 binarized_outer,
@@ -108,9 +104,7 @@ class BinarizedQep:
         assert plan.input_node
 
         dummy_child = BinarizedQep.dummy()
-        binarized_input = BinarizedQep.create_for(
-            plan.input_node, cache_state=cache_state
-        )
+        binarized_input = BinarizedQep.create_for(plan.input_node, cache_state=cache_state)
         return BinarizedQep.pseudo_join(
             plan.node_type,
             binarized_input,
@@ -121,9 +115,7 @@ class BinarizedQep:
         )
 
     @staticmethod
-    def scan(
-        node: str, *, cardinality: int, cost: float, cache_pct: float
-    ) -> BinarizedQep:
+    def scan(node: str, *, cardinality: int, cost: float, cache_pct: float) -> BinarizedQep:
         """Transforms the scan node into its binarized equivalent."""
         return BinarizedQep(False, node, None, None, cardinality, cost, cache_pct)
 
@@ -159,11 +151,7 @@ class BinarizedQep:
         return not self.is_dummy and self.outer_child is None
 
     def is_join(self) -> bool:
-        return (
-            self.outer_child is not None
-            and self.inner_child is not None
-            and not self.inner_child.is_dummy
-        )
+        return self.outer_child is not None and self.inner_child is not None and not self.inner_child.is_dummy
 
     def is_intermediate(self) -> bool:
         return self.inner_child is not None and self.inner_child.is_dummy
@@ -176,9 +164,7 @@ class NodeType(IntEnum):
     Dummy = 4
 
 
-FeaturizedNode = collections.namedtuple(
-    "FeaturizedNode", ["node_type", "encoding", "outer_child", "inner_child"]
-)
+FeaturizedNode = collections.namedtuple("FeaturizedNode", ["node_type", "encoding", "outer_child", "inner_child"])
 
 
 def node_features(node: FeaturizedNode) -> np.ndarray:
@@ -293,9 +279,7 @@ class BaoFeaturizer:
     """
 
     @staticmethod
-    def online(
-        database: pb.Database, *, max_runtime_ms: float = 1000 * 60 * 60
-    ) -> BaoFeaturizer:
+    def online(database: pb.Database, *, max_runtime_ms: float = 1000 * 60 * 60) -> BaoFeaturizer:
         """Infers the featurization from the database.
 
         This featurization strategy trades off high generality for potentially larger
@@ -339,9 +323,7 @@ class BaoFeaturizer:
 
         select_clause = pb.qal.Select.count_star()
         from_clause = pb.qal.From.create_for(largest_tables)
-        cross_product = pb.qal.SqlQuery(
-            select_clause=select_clause, from_clause=from_clause
-        )
+        cross_product = pb.qal.SqlQuery(select_clause=select_clause, from_clause=from_clause)
 
         max_cost = database.optimizer().cost_estimate(cross_product)
 
@@ -478,13 +460,19 @@ class BaoFeaturizer:
         self._min_runtime = min_runtime_ms
         self._max_runtime = max_runtime_ms
 
+        self._min_card_log = np.log(self._min_card)
+        self._max_card_log = np.log(self._max_card)
+        self._min_cost_log = np.log(self._min_cost)
+        self._max_cost_log = np.log(self._max_cost)
+        self._min_runtime_log = np.log(self._min_runtime)
+        self._max_runtime_log = np.log(self._max_runtime)
+
+        # beware of where to pass the log-scaled values and where we need the original ones!
         self._runtime_pipeline = make_pipeline(
             FunctionTransformer(func=np.log1p, inverse_func=np.expm1),
-            MinMaxScaler((self._min_runtime, self._max_runtime)),
+            MinMaxScaler((self._min_runtime_log, self._max_runtime_log)),
         )
-        self._runtime_pipeline.fit(
-            np.asarray([self._min_runtime, self._max_runtime]).reshape(-1, 1)
-        )
+        self._runtime_pipeline.fit(np.asarray([self._min_runtime, self._max_runtime]).reshape(-1, 1))
 
         self._db = database
 
@@ -545,16 +533,16 @@ class BaoFeaturizer:
         """Min-max scales the cardinality estimate of the node."""
         if node.is_dummy:
             return np.zeros(1)
-        scaled = np.log(node.cardinality + 1) - self._min_card
-        scaled /= self._max_card - self._min_card
+        scaled = np.log(node.cardinality + 1) - self._min_card_log
+        scaled /= self._max_card_log - self._min_card_log
         return np.asarray([scaled])
 
     def encode_cost(self, node: BinarizedQep) -> np.ndarray:
         """Min-max scales the cost estimate of the node."""
         if node.is_dummy:
             return np.zeros(1)
-        scaled = np.log(node.cost + 1) - self._min_cost
-        scaled /= self._max_cost - self._min_cost
+        scaled = np.log(node.cost + 1) - self._min_cost_log
+        scaled /= self._max_cost_log - self._min_cost_log
         return np.asarray([scaled])
 
     def encode_cache_pct(self, node: BinarizedQep) -> np.ndarray: